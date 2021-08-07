3a32b99123b832d45add83dc4e470c8459dce249
zooloosoo <87720721+zooloosoo@users.noreply.github.com>
2021-08-07 22:48:09 +0300

    adap

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 5,
+  slidesPerView: 6,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".menu-all");
+const closeBtn = document.querySelector(".close-menu");
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle('is-open');
+});
+closeBtn.addEventListener("click", () => {
+  menu.classList.toggle('is-open');
+});
